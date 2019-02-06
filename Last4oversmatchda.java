02902316f1a38e66c59c8ae60baed73c1abf9233
Saif Ahmed <40402050+Saifahmedprogrminlocallang@users.noreply.github.com>
2019-02-06 18:24:01 +0530

    Add files via upload

--- Last4oversmatchda.java
+++ Last4oversmatchda.java
@@ -38,84 +38,93 @@ public class Last4oversmatchda {
 		over[2] = 3;
 		over[3] = 4;
 
-		Scanner s = new Scanner(System.in);
-		
-		System.out.println("select the bowler to bowl the over"); 
-		System.out.println("1.	name[0]  Kirat Boli");
-		System.out.println("2.	name[1]  N.S. Nodhi");
-		System.out.println("3.  name[2]  R Rumrah");
-		System.out.println("4.  name[3]  Shashi Henra");
-		
-		System.out.println("User Input - 1, 2, 3, 4");	
-		  
-		int option = 0;
-		int bowlerselected;
-/*		option = Integer.parseInt(s.nextLine().trim());
-		System.out.println("option" + option);
-		
-		int bowlerselected;
-		bowlerselected=1;*/
-		
-		int i;
-		i=0;
-		int icntr;
-		icntr=0;
-
-		Bowlingprogrambowlers_Bprgb bowlingprogrammatchsequence = new Bowlingprogrambowlers_Bprgb(); 
-		int bowleroutcome[] = new int[900];
-		int overorderforeverybowler[]=new int[4];
-		
-		//generate bowling for the bowlers
-		while (icntr<4){
+		try { 
 			
-			System.out.println("Starting match -- generate match sequence and statistics - ");
-			icntr=icntr+1;
-			option = Integer.parseInt(s.nextLine().trim());
+		
+			Scanner s = new Scanner(System.in);
 			
-			overorderforeverybowler[icntr]=option;
+			System.out.println("select the bowler to bowl the over"); 
+			System.out.println("0.	name[0]  Kirat Boli");
+			System.out.println("1.	name[1]  N.S. Nodhi");
+			System.out.println("2.  name[2]  R Rumrah");
+			System.out.println("3.  name[3]  Shashi Henra");
+			
+			System.out.println("User Input - 0, 1, 2, 3");	
+			  
+			int option;
+			option=0;
+			int optioncontinue;
+			optioncontinue=0;
+			int bowlerselected;
+			int equationsrunsscoredwicketsremaining[] = new int[2];
+			//equationsrunsscoredwicketsremaining[0]=37;
+			//equationsrunsscoredwicketsremaining[1]=4;
+			
+	/*		option = Integer.parseInt(s.nextLine().trim());
 			System.out.println("option" + option);
 			
-			bowlerselected=option;
-			// write to the "bowler"+bowler+"sequenceandpatternfile.txt files 4 files should be generated
-			bowlingprogrammatchsequence.Matchgeneratebbowlersovers(bowlerselected, icntr);
-		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PLAY MATCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		System.out.println("Number ----- press any number to continue ----- ");
-		option = Integer.parseInt(s.nextLine().trim());
-		
-		//PlayGame playgameplaymatchMatch947119429741144254119 = new PlayGame(); 
-		if ((option>0) && (option<=4)) {
-			int equationsrunsscoredwicketsremaining[] = new int[2];
-			equationsrunsscoredwicketsremaining=bowlingprogrammatchsequence.Matchplayball(overorderforeverybowler[option],icntr); //bowler bowling order and over
+			int bowlerselected;
+			bowlerselected=1;*/
+			
+			int i;
+			i=0;
+			int icntr;
+			icntr=0;
+	
+			Bowlingprogrambowlers_Bprgb bowlingprogrammatchsequence = new Bowlingprogrambowlers_Bprgb(); 
+			int bowleroutcome[] = new int[12];
+			int overorderforeverybowler[]=new int[4];
+			
+			//generate bowling for the bowlers
+			while (icntr<4) {
+				
+				System.out.println("Starting match -- generate match sequence and statistics - 5 to exit");
 
-			//checkequation
-			if ((equationsrunsscoredwicketsremaining[0]>=37) && (equationsrunsscoredwicketsremaining[1]<4) && (icntr<=4)) {
-				System.out.println("Lengaburu wins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);	
-			} else if ((equationsrunsscoredwicketsremaining[0]==37) && (equationsrunsscoredwicketsremaining[1]<4) && (icntr<=4)) {
-				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ITS A TIE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);	
-			} else if ((equationsrunsscoredwicketsremaining[0]>=37) && (equationsrunsscoredwicketsremaining[1]<4) && (icntr==4)) {
-				System.out.println("Lengaburu Wins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);	
-			} else if ((equationsrunsscoredwicketsremaining[0]<37) && (equationsrunsscoredwicketsremaining[1]>4) && (icntr==4)) {
-				System.out.println("Enchai Wins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);	
-			} else if ((equationsrunsscoredwicketsremaining[0]<37) && (equationsrunsscoredwicketsremaining[1]<=4) && (icntr==4)) {
-				System.out.println("Enchai Wins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);	
+				option = Integer.parseInt(s.nextLine().trim());
+				
+				if (icntr==4) {
+					//icntr=icntr-1;
+					System.exit(0);
+				} else { 
+					overorderforeverybowler[icntr]=option;
+				}
+				System.out.println("option" + option);
+				
+				bowlerselected=option;
+				// write to the "bowler"+bowler+"sequenceandpatternfile.txt files 4 files should be generated
+				bowleroutcome= bowlingprogrammatchsequence.Matchgeneratebbowlersovers(bowlerselected, icntr);
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PLAY MATCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			System.out.println("Number ----- press any number to continue 5 to exit----- ");
+			optioncontinue = Integer.parseInt(s.nextLine().trim());
+			
+			//PlayGame playgameplaymatchMatch947119429741144254119 = new PlayGame(); 
+			if ((option>=0) && (option<4)) {
+
+				equationsrunsscoredwicketsremaining=bowlingprogrammatchsequence.Matchplayball(bowleroutcome,overorderforeverybowler[option],icntr,option); //bowler bowling order and over
+	
+				if (bowlingprogrammatchsequence.CheckEQCcheckequation(equationsrunsscoredwicketsremaining,icntr)==1) {
+					System.out.println("Match Ends - Lengaburu wins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				} else if (bowlingprogrammatchsequence.CheckEQCcheckequation(equationsrunsscoredwicketsremaining,icntr)==0) {
+					System.out.println("Match Ends - ITS A TIE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				} else if (bowlingprogrammatchsequence.CheckEQCcheckequation(equationsrunsscoredwicketsremaining,icntr)==2) {
+					System.out.println("Match Ends - Enchai Wins !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				}
+
+			} 
+				icntr=icntr+1;
 			}
 			
+			System.out.println("Number ----- press any number to continue 5 to exit----- ");
+			optioncontinue = Integer.parseInt(s.nextLine().trim());
+			if (optioncontinue==5){		
+				System.exit(0);
+			}
 			
-		} else if (option==5){		
-			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		}
-		
 	}
 
-
-			
 	
 	}
 
