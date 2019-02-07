54cb0a176a37e27a1c9558f13d61b37da73c05a2
Saif Ahmed <40402050+Saifahmedprogrminlocallang@users.noreply.github.com>
2019-02-07 19:19:48 +0530

    Add files via upload

---
+++ PlayGame.java
@@ -0,0 +1,46 @@
+package com.testing.ahmed;
+
+public class PlayGame {
+
+	public static class Match {
+
+		static int Matchscore;
+		static int Matchwickets;
+		static int ballsbowled;
+
+		static int Currentscore;
+		static int Currentwickets;		
+		static String Matchstatistics;
+		//static int matchesplayed;
+		static int oversbowledbyeverybowlerinthematch[]={0,0,0,0};
+		static int Currentmatchbowlerordersequence[]= {1,2,3,4};
+
+		int i;
+
+		public void intitializeMatch() {
+			Currentscore=0;
+			Currentwickets=0;
+			Matchscore=40; // read from properties file 
+			Matchwickets=3;
+			ballsbowled=0;
+			Matchstatistics="D:\\Kirkaet\\MatchStatsandmatchstatistics.xml";
+			System.out.println("Match sequence numbers data for bowlers generated for 2 matches only");
+			//System.out.println("Matchstatistics Match Matchesplayed- "+matchesplayed);
+			//matchesplayed=0; // read from property file
+			i=0;
+			/*******
+			for(i=1;i<=4;i++) {
+				Currentmatchbowlerordersequence[i]=Extrafunc.readcounterfromfile("oversbowled"+i, Matchstatistics);
+			}
+			*******/
+	
+			
+		}
+
+		
+		//TWO COUNTERS - Match
+		// AND oversbowled-i
+	}
+
+
+}
